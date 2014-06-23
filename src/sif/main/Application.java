1a03cc763dfafe7a5c84349bddbfdd80d0d87001
krauswg <a@b.c>
2014-06-23 14:04:34 +0200

    reworked the cli / argument parsing, according to kuleszdl/SIFEI#57

--- src/sif/main/Application.java
+++ src/sif/main/Application.java
@@ -1,20 +1,11 @@
 package sif.main;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.StringReader;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
 
 import sif.IO.ReportFormat;
-import sif.IO.xml.SifMarshaller;
-import sif.frontOffice.FrontDesk;
-import sif.model.policy.Policy;
-import sif.model.policy.PolicyList;
 
 /***
  * This is the application class of the Spreadsheet Inspection Framework.
@@ -25,194 +16,118 @@ import sif.model.policy.PolicyList;
  */
 public class Application {
 	private static boolean DEBUG = false;
+	private static File parentFolder = null;
+	private static final String DEBUGFILENAME = "debug";
+	private static final String MODESOCKET = "socket";
+	private static final String MODEFILE = "file";
+	
+	public static boolean isDebug(){
+		return DEBUG;
+	}
+	
+	private static void checkParentFolder(){
+		try {
+			parentFolder = new File(
+					Application.class.getProtectionDomain()
+					.getCodeSource().getLocation()
+					.toURI().getPath()
+			).getParentFile();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void checkDebug(){
+		File debugFile = new File(parentFolder, DEBUGFILENAME);
+		if (debugFile.exists()){
+			DEBUG = true;
+		}
+	}
 	/**
 	 * @param args
 	 * 
 	 */
 	public static void main(String[] args) {
+		checkParentFolder();
+		checkDebug();
+		if (args.length < 2){
+			printUsageAndExit("Not enough parameters.");
+		}
 
-		// Check for correct command line parameters
-		if (args != null && args.length >= 1) {
 
+		if (args[0].equalsIgnoreCase(MODESOCKET)){
 			try {
-				if (args.length >= 2){
-					if (args[1].equalsIgnoreCase("-debug")){
-						DEBUG = true;
-					}
-				} else if (args.length == 1){
-					File debugFile = new File("C:\\Spreadsheet Inspection Framework\\debug");
-					if (debugFile.exists()){
-						DEBUG = true;
-					}
-					}
-				int port = Integer.parseInt(args[0]);
-
-				// Try and open a socket connection to the server
-				Socket clientSocket = null;
-
-				try {
-
-					clientSocket = new Socket(InetAddress.getLoopbackAddress(),
-							port);
-
-					while (true) {
-
-						/*
-						 * Read the policy file
-						 */
-						String policyFile = Utils.readString(clientSocket);
-
-						if (DEBUG) {
-							BufferedWriter out = new BufferedWriter(new FileWriter(new File("C:\\Spreadsheet Inspection Framework\\dynPol.xml")));
-							out.write(policyFile);
-							out.close();
-//							return;
-						}
-						/*
-						 * Read the spreadsheet file
-						 */
-						byte[] spreadsheetContent = Utils
-								.readBytes(clientSocket);
-						File spreadsheetFile = Utils
-								.writeToTempFile(spreadsheetContent);
-
-						/*
-						 * Generate the report
-						 */
-						String requestName = "Programmatic Request";
-
-						FrontDesk desk = FrontDesk.getInstance();
-
-						PolicyList policyList = SifMarshaller
-								.unmarshal(new StringReader(policyFile));
-						
-						Policy policy = policyList.getDynamicPolicy();
-						if (policy == null){
-							policy = new Policy();
-						}
-						if (policyList.getFormulaComplexityRule() != null){
-							policy.add(policyList.getFormulaComplexityRule());
-						}
-						if (policyList.getNoConstantsRule() != null){
-							policy.add(policyList.getNoConstantsRule());
-						}
-						if (policyList.getReadingPolicyRule() != null){
-							policy.add(policyList.getReadingPolicyRule());
-						}
-						if (policyList.getSanityPolicyRule() != null){
-							policy.add(policyList.getSanityPolicyRule());
-						}
-
-						desk.createAndRunDynamicInspectionRequest(requestName,
-								spreadsheetFile, policy);
-
-						String xmlReport = desk
-								.createInspectionReport(ReportFormat.XML);
-						if (DEBUG){
-							BufferedWriter out = new BufferedWriter(new FileWriter(new File("C:\\Spreadsheet Inspection Framework\\findings.xml")));
-							out.write(xmlReport);
-							out.close();
-						}
-						/*
-						 * Send the report
-						 */
-						Utils.writeString(clientSocket, xmlReport);
-
-					}
-
-				} catch (IOException e){
-					// silently ignore the IOException when it is closed
-				} catch (Throwable e) {
-					if (DEBUG){
-						// show a window with the exceptions from the application
-						e.printStackTrace();
-						for (Throwable e2 : e.getSuppressed())
-							e2.printStackTrace();
-						DebugConsole con = new DebugConsole();
-						con.addStackTrace(e);
-						if (e.getCause() != null)
-							con.addStackTrace(e.getCause());
-						for (Throwable e2 : e.getSuppressed()){
-							con.addStackTrace(e2);
-							if (e2.getCause() != null)
-								con.addStackTrace(e2.getCause());
-						}
-						// Close the socket so SIFEI can continue to work, or rather fail
-						// the parsing due to no root element
-						// TODO: report why it failed
-						if (!clientSocket.isClosed()){
-							try {
-								clientSocket.close();
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-						new Thread(con).start();
-					}
-				}
-
+				int port = Integer.parseInt(args[1]);
+				RunSocketMode client = new RunSocketMode(port);
+				client.blockingListening();
 			} catch (NumberFormatException e) {
-
-				// Parameter was not an integer, just exit silently
-				// as we do not expect a human to call this jar
-				// System.out.println("Wrong parameter");
-
+				e.printStackTrace();
+				printUsageAndExit("Error while parsing the portnumber");
 			}
-
-		} else {
-			// debugging procedure, reads the policy.xml and runs on checking.xls
-			// in the project root (eclipse) or working directory (jar)
+		} else if (args[0].equalsIgnoreCase(MODEFILE)){
+			String errors = "";
+			if (args.length < 4){
+				errors += "Not enough parameters for the file mode.";
+				printUsageAndExit(errors);
+			}
+			ReportFormat format = null;
 			try {
-
-
-				StringBuilder build = new StringBuilder();
-				BufferedReader in = new BufferedReader(new FileReader(new File("policy.xml")));
-				String akt = null;
-				while ((akt = in.readLine()) != null){
-					build.append(akt);
-				}
-				in.close();
-
-				PolicyList policyList = SifMarshaller
-						.unmarshal(new StringReader(build.toString()));
-
-				Policy policy = policyList.getDynamicPolicy(); 
-				if (policy == null){
-					policy = new Policy();
-				}
-				if (policyList.getFormulaComplexityRule() != null){
-					policy.add(policyList.getFormulaComplexityRule());
-				}
-				if (policyList.getNoConstantsRule() != null){
-					policy.add(policyList.getNoConstantsRule());
-				}
-				if (policyList.getReadingPolicyRule() != null){
-					policy.add(policyList.getReadingPolicyRule());
-				}
-				if (policyList.getSanityPolicyRule() != null){
-					policy.add(policyList.getSanityPolicyRule());
-				}
-				
-				FrontDesk desk = FrontDesk.getInstance();
-				
-				desk.createAndRunDynamicInspectionRequest("Debug",
-						new File("checking.xls"), policy);
-
-				String xmlReport = desk
-						.createInspectionReport(ReportFormat.XML);
-
-				
-				System.out.println(xmlReport);
-
-			} catch (Exception e){
+				format = ReportFormat.valueOf(args[1].toUpperCase());
+			} catch (IllegalArgumentException e){
+				errors += "Unknown report format, currently supported formats are: ";
+				errors += Arrays.toString(ReportFormat.values()) + "\n";
+			}
+			File policyFile = new File(args[2]);
+			if (!policyFile.exists()){
+				errors += "Policy file not found at " + args[2] + "\n";
+			}
+			
+			File spreadsheetFile = new File(args[3]);
+			if (!spreadsheetFile.exists()){
+				errors += "Spreadsheet file not found at " + args[3] + "\n";
+			}
+			
+			if (!errors.isEmpty()){
+				printUsageAndExit(errors);
+			}
+			
+			try {
+				String report = RunFileMode.execute(format, policyFile, spreadsheetFile);
+				System.out.println(report);
+			} catch (Exception e) {
 				e.printStackTrace();
 				for (Throwable t : e.getSuppressed()){
 					t.printStackTrace();
 				}
 			}
-
+			
 		}
+		
 
+	}
+	
+	/**
+	 * Prints the reason to the standard output followed by the usage syntax and exits with the
+	 * exit code -1
+	 * @param reason why it was invalid
+	 */
+	private static void printUsageAndExit(String reason){
+		String sb = "";
+		if (reason != null){
+			sb += reason + "\n";
+		}	
+		sb += "Usage: ";
+		sb += "sif [MODE] [PARAMETERS]. ";
+		sb += "Currently supported modes:\n";
+		sb += "sif socket [portnumber]\n";
+		sb += "sif file [html | xml] [path/to/policyFile] [path/to/spreadsheetFile]\n";
+		if (isDebug()){
+			sb += "To disable the debug mode remove or rename the file called 'debug' in the "
+					+ "same folder as the jar";
+		} else {
+			sb += "To enable the debug mode place a file called 'debug' in the same folder as the jar";
+		}
+		System.out.print(sb);
+		System.exit(-1);
 	}
 }
---
+++ src/sif/main/RunFileMode.java
@@ -0,0 +1,54 @@
+package sif.main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.StringReader;
+
+import sif.IO.ReportFormat;
+import sif.IO.xml.SifMarshaller;
+import sif.frontOffice.FrontDesk;
+import sif.model.policy.Policy;
+import sif.model.policy.PolicyList;
+
+public class RunFileMode {
+
+	
+	public static String execute(ReportFormat outputFormat, File policyFile, File spreadsheetFile) throws Exception{
+		StringBuilder build = new StringBuilder();
+		BufferedReader in = new BufferedReader(new FileReader(policyFile));
+		String akt = null;
+		while ((akt = in.readLine()) != null){
+			build.append(akt);
+		}
+		in.close();
+
+		PolicyList policyList = SifMarshaller
+				.unmarshal(new StringReader(build.toString()));
+
+		Policy policy = policyList.getDynamicPolicy(); 
+		if (policy == null){
+			policy = new Policy();
+		}
+		if (policyList.getFormulaComplexityRule() != null){
+			policy.add(policyList.getFormulaComplexityRule());
+		}
+		if (policyList.getNoConstantsRule() != null){
+			policy.add(policyList.getNoConstantsRule());
+		}
+		if (policyList.getReadingPolicyRule() != null){
+			policy.add(policyList.getReadingPolicyRule());
+		}
+		if (policyList.getSanityPolicyRule() != null){
+			policy.add(policyList.getSanityPolicyRule());
+		}
+		
+		FrontDesk desk = FrontDesk.getInstance();
+		
+		desk.createAndRunDynamicInspectionRequest("File mode request",
+				spreadsheetFile, policy);
+
+		return desk.createInspectionReport(outputFormat);
+	}
+
+}
