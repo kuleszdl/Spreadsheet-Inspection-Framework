d63faed78307c1b23a7b68346becd2ce2aee79de
Daniel Kulesz <daniel.kulesz@informatik.uni-stuttgart.de>
2016-02-15 02:52:01 +0100

    Enhance violation messages for rules "multiplesameref" and "constants"

--- src/sif/model/violations/single/MultipleSameRefSingleViolation.java
+++ src/sif/model/violations/single/MultipleSameRefSingleViolation.java
@@ -1,6 +1,7 @@
 package sif.model.violations.single;
 
 import sif.model.elements.basic.reference.AbstractReference;
+import sif.utilities.Translator;
 
 public class MultipleSameRefSingleViolation extends GenericSingleViolation{
 	private int count;
@@ -8,14 +9,34 @@ public class MultipleSameRefSingleViolation extends GenericSingleViolation{
 	@Override
 	public String getDescription() {
 		StringBuilder description = new StringBuilder();
-		description.append("The reference ");
+		
+		description.append(Translator.instance.tl("PolicyMultipleSameRef.0010", "The formula in this cells cell refers"));
+		description.append(" ");
+		description.append(count);
+		description.append(" ");
+		description.append(Translator.instance.tl("PolicyMultipleSameRef.0011","times in a row to the cell"));
+		description.append(" ");
 		description.append(getCausingElement().getStringRepresentation());
-		description.append(" appears " + count + " times");
+		description.append(". ");
+		description.append(".");
+		
 		if (getCausingElement() instanceof AbstractReference){
-			description.append(" in ");
+			description.append(" ");
+			description.append(Translator.instance.tl("PolicyMultipleSameRef.0012","The references are separated by"));
+			description.append(": ");
 			description.append(((AbstractReference) getCausingElement()).getContainer().getStringRepresentation());
 		}
-		description.append(".");
+		
+//		description.append("The reference ");
+//		description.append(getCausingElement().getStringRepresentation());
+//		description.append(" appears " + count + " times");
+//		if (getCausingElement() instanceof AbstractReference){
+//			description.append(" in ");
+//			description.append(((AbstractReference) getCausingElement()).getContainer().getStringRepresentation());
+//		}
+//		description.append(".");
+		
+		
 		return description.toString();
 	}
 
